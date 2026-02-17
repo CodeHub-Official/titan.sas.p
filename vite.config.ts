66de1c68b4aab7993761aa466a153d4f1f043d60
Codehub <silemtarek2019@gmail.com>
2026-02-17 16:53:27 +0200

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'client', // هذا السطر يخبر Vite أن ملف index.html موجود داخل مجلد client
+  root: 'client',
   build: {
     outDir: '../dist/public',
     emptyOutDir: true,
